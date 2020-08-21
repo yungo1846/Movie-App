7c53b5b3936c69fb2e348feb31e44ba6f3b0b8f1
yungo1846 <yungo1468@naver.com>
2020-08-21 17:59:15 +0900

    Updates

---
+++ precache-manifest.2202628ad94a70dd89f8ac3a14e4b1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5461aafdafe906fc2ade6c8ae2fa9f7",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "5e25eb2abfc8f96a8d6b",
+    "url": "/movie-app/static/css/main.e1ca4300.chunk.css"
+  },
+  {
+    "revision": "08362894accf5635f13d",
+    "url": "/movie-app/static/js/2.6d4ff6d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-app/static/js/2.6d4ff6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e25eb2abfc8f96a8d6b",
+    "url": "/movie-app/static/js/main.f721f304.chunk.js"
+  },
+  {
+    "revision": "1bbc20ee722c49fe6354",
+    "url": "/movie-app/static/js/runtime-main.9d774eac.js"
+  }
+]);
